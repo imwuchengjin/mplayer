f50f77eadfde004cdbe91501774b797835513854
imwuchengjin <imwuchengjin@gmail.com>
2016-04-16 15:20:46 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "qtmplayer.h"
+#include <QApplication>
+#include <QTextCodec>
+
+int main(int argc, char *argv[])
+{
+	QApplication app(argc, argv);
+
+	QtMPlayer mplayer;
+	mplayer.show();
+	return app.exec();
+}
